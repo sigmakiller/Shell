aac6346367ecb6c7f91efb248ebd2142823a7dc6
SIGMA KILLER <137909055+sigmakiller@users.noreply.github.com>
2026-05-19 21:41:36 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -4,9 +4,9 @@ def main():
    
     
     sys.stdout.write("$ ")
-    
-    command=input()
-    print(f"{command}: command not found")
+    while True:
+        command=input()
+        print(f"{command}: command not found")
     
     pass
 
