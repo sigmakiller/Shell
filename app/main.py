0b680925afc2dc9375badc9340b1a79e084f36a9
SIGMA KILLER <137909055+sigmakiller@users.noreply.github.com>
2026-05-19 21:50:23 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -8,7 +8,8 @@ def main():
         sys.stdout.write("$ ")
         command=input()
         print(f"{command}: command not found")
-    
+        if command=="exit":
+            break
     pass
 
 
